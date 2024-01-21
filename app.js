ea8c932ab666a931fc054b1c03039b5fb138c64b
Harry <152967791+0Harrison@users.noreply.github.com>
2024-01-21 22:55:53 +0100

    harry's assignment

---
+++ app.js
@@ -0,0 +1,114 @@
+//******OBJECT*********//
+
+//create a variable containing a key
+let myName = 'Ogbonnia'
+
+let young = {}
+//use the variable to dynamically assign a value to thhe object
+young[myName]="Harrison";
+console.log(young)
+
+//******NO 2******/
+
+let main = {
+    address: "no 3 seaman avenue",
+    gender: "male",
+    age: 23,
+    school : "fstc",
+}
+
+delete main.address
+delete main.gender
+delete main.school
+
+console.log(main)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
